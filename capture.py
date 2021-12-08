481f8efee618853886bb9e3d5b7ee5c48410ec4a
Prishu <prishupandey01142@gmail.com>
2021-12-08 21:53:30 +0530

    Detecting Moving Objects from the Webcam

--- capture.py
+++ capture.py
@@ -1,27 +1,49 @@
 import cv2, time
 
+first_frame = None
+
 video = cv2.VideoCapture(0)
 
-a =1
 
 while True:
-  a = a+1
+ 
   check, frame = video.read()
-
-  print(check)
-  print(frame)
+  status=0
 
   gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  gray = cv2.GaussianBlur(gray,(21,21),0)
   #time.sleep(3)
+  #gray is basically a numpy array
 
-  cv2.imshow("Capturing",gray)
+  if first_frame is None:
+    first_frame = gray
+    continue
+
+  #now we will apply delta frames
+  delta_frame = cv2.absdiff(first_frame,gray)
+
+  thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+  thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+  (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+  for contour in cnts:
+    if cv2.contourArea(contour) < 1000:
+      continue
+
+    (x, y, w, h)=cv2.boundingRect(contour)
+    cv2.rectangle(frame, (x, y),(x+w,y+h),(0,255,0), 3)
+
+  cv2.imshow("Gray Frames",gray)
+  cv2.imshow("Delta Frames",delta_frame)
+  cv2.imshow("Threshold Frame",thresh_frame)
+  cv2.imshow("Color Frame",frame)
 
   key = cv2.waitKey(1)
 
   if key==ord('q'):
     break
-
-print(a)
 
 video.release()
 cv2.destroyAllWindows()
